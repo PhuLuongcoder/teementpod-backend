585001a5bb1766d2ecdfae70ec729495157598c3
Lương Bá Phú <luongbaphu326@gmail.com>
2026-06-01 06:46:38 +0700

    Clear all middleware routes in middlewares.ts
    Removed all middleware routes and left the structure empty.

--- src/api/middlewares.ts
+++ src/api/middlewares.ts
@@ -1,37 +1,4 @@
-import { defineMiddlewares } from "@medusajs/framework/http";
-import cors from "cors";
-
+// Chỉ để lại rỗng như thế này
 export default defineMiddlewares({
-  routes: [
-    {
-      // 1. Dành cho cổng Seller
-      matcher: "/partner/*",
-      // Bỏ luôn chữ method để nó tự động bắt mọi request, bao gồm cả OPTIONS
-      middlewares: [
-        cors({
-          origin: [
-            "https://seller.teementpod.us",
-            "https://teementpod.us",
-            "https://www.teementpod.us",
-            "http://localhost:3001"
-          ], 
-          credentials: true, 
-        }),
-      ],
-    },
-    {
-      // 2. Dành cho cổng Admin
-      matcher: "/admin/*",
-      middlewares: [
-        cors({
-          origin: [
-            "https://api.teementpod.us",
-            "https://seller.teementpod.us",
-            "http://localhost:3000"
-          ],
-          credentials: true,
-        }),
-      ],
-    }
-  ],
+  routes: [],
 });
